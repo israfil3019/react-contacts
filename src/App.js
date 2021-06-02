bff2ef93d27d0ebc532095d4b0af4ded611f66be
israfil <israfil3019@gmail.com>
2021-06-02 23:45:49 +0300

    add form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import "./App.css";
+import { useState } from "react";
+import Contacts from "./components/contacts/Contacts";
+import Form from "./components/form/Form";
+
+const initialValues = { username: "", phoneNumber: "", gender: "NO INFO!" };
 
 function App() {
-  return <div className="App"></div>;
+  const [info, setInfo] = useState(initialValues);
+
+  return (
+    <div className="App">
+      <Form className="form" info={info} setInfo={setInfo} />
+      <Contacts className="contacts" />
+    </div>
+  );
 }
 
 export default App;
